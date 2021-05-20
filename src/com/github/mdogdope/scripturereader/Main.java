8bcebc16e7d40fdcd8d5276f650e836f6ed7b13e
Michael Heston <m45t3rhest@gmail.com>
2021-05-20 16:17:41 -0600

    Started work on the text to speech function

--- src/com/github/mdogdope/scripturereader/Main.java
+++ src/com/github/mdogdope/scripturereader/Main.java
@@ -10,9 +10,22 @@ public class Main {
 	
 	public static void main(String[] args) {
 		
-		Vector<BookData> chData = getChData();
+		
+//		Vector<BookData> chData = getChData();
 //		new Scraper(chData);
 //		new Parser(chData);
+
+		/*
+		 * ####### Ideas #######
+		 * Show short codes for the books
+		 * Let user enter book "name ch [end] [verse [end]]"
+		 * On first start have user set speed/pitch/volume
+		 * Let user enter stops to take a break in reading.
+		 * - at break let user continue or replay last or pick spot(include exit option).
+		 * - let user quit at any break
+		 * - adjust speed/pitch/volume. Shows current levels for reference.
+		 * 
+		 */
 	}
 	
 	private static Vector<BookData> getChData(){
--- src/com/github/mdogdope/scripturereader/Reader.java
+++ src/com/github/mdogdope/scripturereader/Reader.java
@@ -1,5 +1,53 @@
 package com.github.mdogdope.scripturereader;
 
-public class Reader {
+import java.util.Arrays;
 
+import com.sun.speech.freetts.Voice;
+import com.sun.speech.freetts.VoiceManager;
+import com.sun.speech.freetts.en.us.CMULexicon;
+import com.sun.speech.freetts.lexicon.Lexicon;
+
+public class Reader {
+	private Voice voice = null;
+
+	public Reader() {
+		
+		float rate = 150;
+		float pitch = 150;
+		float volume = 1;
+		
+		Lexicon custom = new CMULexicon();
+		
+		System.out.println(custom);
+		
+		System.setProperty("freetts.voices", "com.sun.speech.freetts.en.us.cmu_us_kal.KevinVoiceDirectory");
+		this.voice = VoiceManager.getInstance().getVoice("kevin16");
+		if (this.voice != null) {
+			this.voice.allocate();
+		}
+		this.voice.setRate(rate);
+		this.voice.setPitch(pitch);
+		this.voice.setVolume(volume);
+		
+		
+		System.out.println(Arrays.toString(this.voice.getLexicon().getPhones("Nephi", null)));
+		this.voice.speak("Nephi");
+		
+	}
+	
+	public void setRate(Integer rate) {
+		this.voice.setRate(rate);
+	}
+	
+	public void setPitch(Float pitch) {
+		this.voice.setPitch(pitch);
+	}
+	
+	public void setVolume(Float volume) {
+		this.voice.setVolume(volume);
+	}
+	
+	public void read(String text) {
+		this.voice.speak(text);
+	}
 }
