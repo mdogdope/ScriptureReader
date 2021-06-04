87f7af01929a8ff29d81134dc0fe825798d628df
Michael Heston <m45t3rhest@gmail.com>
2021-06-04 14:52:21 -0600

    Did stuff

---
+++ src/com/github/mdogdope/scripturereader/Block.java
@@ -0,0 +1,9 @@
+package com.github.mdogdope.scripturereader;
+
+public class Block {
+	public String book = new String();
+	public Integer chapter = 0;
+	public Integer start = 0;
+	public Integer end = 0;
+}
+
--- src/com/github/mdogdope/scripturereader/Generator.java
+++ src/com/github/mdogdope/scripturereader/Generator.java
@@ -3,33 +3,19 @@ package com.github.mdogdope.scripturereader;
 import java.util.Vector;
 
 public class Generator {
-		
-	private Vector<Book> sections = new Vector<Book>();
+
+	private Vector<Block> blocks = new Vector<Block>();
 	
-	public void add(String name, Integer chapter, Integer start, Integer end) {
-		this.sections.add(new Book(name, chapter, start, end));
+	public void addBlock(Block block) {
+		this.blocks.add(block);
 	}
 	
-	public void remove() {
-		
-	}
-	
-	public Vector<Book> getAll() {
-		return this.sections;
-	}
-	
-	private String transCode(String code) {
-		String name = new String();
-		final String[] names = {"1 Nephi", "2 Nephi", "Jacob", "Enos", "Jarom", "Omni", "Words of Mormon", "Mosiah", "Alma", "Helaman", "3 Nephi", "4 Nephi", "Mormon", "Ether", "Moroni"};
-		final String[] codes = {"1-ne", "2-ne", "jacob", "enos", "jarom", "omni", "w-of-m", "mosiah", "alma", "hel", "3-ne", "4-ne", "morm", "ether", "moro"};
-		
-		for(int i = 0; i < codes.length; i++) {
-			if(code.equals(codes[i])) {
-				name = names[i];
-				break;
-			}
+	public void getBlock(int index) {
+		if(this.blocks.size() < index) {
+			System.out.println("Can't get block. Invalid index.");
+			return;
 		}
-		return name;
 	}
 	
 }
+
