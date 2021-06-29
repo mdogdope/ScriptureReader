f4aef7448a6500086aa406cc9f240b57c6ac99ea
Michael Heston <m45t3rhest@gmail.com>
2021-06-29 16:29:15 -0600

    Started working on imporving the ui

--- src/com/github/mdogdope/scripturereader/Main.java
+++ src/com/github/mdogdope/scripturereader/Main.java
@@ -1,5 +1,7 @@
 package com.github.mdogdope.scripturereader;
 
+import java.util.regex.Pattern;
+
 public class Main {
 
 	public static void main(String[] args) {
@@ -11,45 +13,22 @@ public class Main {
 		screen.showMainMenu();
 
 		String choice = new String("q");
+		Pattern pat = Pattern.compile("[1-9]");
 
 		do {
 			choice = screen.getInput();
-
-			if (choice.equals("?")) {
-				screen.showBlockHelp();
-			}else if (choice.toLowerCase().equals("split")) {
-				gen.addSplit();
-			}else if(choice.toLowerCase().equals("show") || choice.toLowerCase().equals("list")) {
-				gen.listBlocks();
-			}else if(choice.equals("export")) {
-				gen.exportBlock("test.txt", settings);
-			}else if (choice.length() > 1 && choice.contains(" ")) {
-				String[] params = choice.split(" ");
+			
+			String[] parts = choice.split(" ");
+			
+			if(parts[0].equals("?")) {
 				
-				if(params[0].equals("r")){
-					gen.removeBlock( Integer.parseInt(params[1]));
-				}else if (params.length == 3) {
-					gen.addBlock(new Block(params[0], Integer.parseInt(params[1]), Integer.parseInt(params[2])));
-				} else {
-					gen.addBlock(new Block(params[0], Integer.parseInt(params[1]), Integer.parseInt(params[2]),
-							Integer.parseInt(params[3])));
-				}
+			}
+			
+			if(pat.matcher(parts[0]).matches()) {
+				
 			}
 
 		} while (!choice.toLowerCase().equals("q"));
 
-//		Vector<BookData> chData = getChData();
-//		new Scraper(chData);
-//		new Parser(chData);
-
-		/*
-		 * ####### Ideas ####### Show short codes for the books Let user enter book
-		 * "name ch [end] or [start end]" On first start have user set
-		 * speed/pitch/volume Let user enter stops to take a break in reading. - at
-		 * break let user continue or replay last or pick spot(include exit option). -
-		 * let user quit at any break - adjust speed/pitch/volume. Shows current levels
-		 * for reference.
-		 * 
-		 */
 	}
 }
